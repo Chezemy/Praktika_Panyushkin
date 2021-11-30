1462a7e6ea762e57b1f7733c4b090d02b8ce2852
Pavel Panyushkin <40539112+Chezemy@users.noreply.github.com>
2021-11-30 12:06:34 +0300

    Update main.py

--- OpenCV/neural_network/main.py
+++ OpenCV/neural_network/main.py
@@ -18,7 +18,7 @@ output_movie=cv2.VideoWriter('output.mp4',fourcc,29.97,(width,height),True)
 
 def load_movie(path_input,path_output):
   global input_movie, lenght, height, width,output_movie
-  input_movie = cv2.VideoCapture("sample.mp4")
+  input_movie = cv2.VideoCapture(path_input)
   lenght = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))
   height = int(input_movie.get(cv2.CAP_PROP_FRAME_HEIGHT))
   width = int(input_movie.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -28,7 +28,6 @@ def load_movie(path_input,path_output):
 def learn_faces():
   photos=os.listdir(path_photos)
   for photo in photos:
-
     try:
       image=face_recognition.load_image_file(path_photos+photo)
       face_encoding=face_recognition.face_encodings(image)[0]
@@ -47,37 +46,37 @@ learn_faces()
 for i in range(len(known_faces)):
   print(known_names[i])
   print(lenght)
-
+  
 while True:
   ret,frame=input_movie.read()
   frame_number +=1
-if not ret:
-  break
-rgb_frame = frame[:,:,::-1]
+  if not ret:
+    break
+  rgb_frame = frame[:,:,::-1]
 
-face_locations=face_recognition.face_locations(rgb_frame)
-face_encodings=face_recognition.face_encodings(rgb_frame,face_locations)
-face_names=[]
-for face_encoding in face_encodings:
-  match = face_recognition.compare_faces(known_faces, face_encoding,tolerance=0.50)
-  name = None
-for m in range(len(match)):
-  if match[m]:
-    name = known_names[m]
-    print(name)
-    face_names.append(name)
-  break
+  face_locations=face_recognition.face_locations(rgb_frame)
+  face_encodings=face_recognition.face_encodings(rgb_frame,face_locations)
+  face_names=[]
+  for face_encoding in face_encodings:
+    match = face_recognition.compare_faces(known_faces, face_encoding,tolerance=0.50)
+    name = None
+    for m in range(len(match)):
+      if match[m]:
+        name = known_names[m]
+        print(name)
+        face_names.append(name)
+        break
 
-for (top,right,bottom,left), name in zip(face_locations, face_names):
-  if not name:
-    continue
-  cv2.rectangle(frame,(left,top),(right,bottom),(0, 0, 255), 2)
-  cv2.rectangle(frame,(left,bottom-25),(right,bottom), (0,0,255), cv2.FILLED)
-  font =cv2.FONT_HERSHEY_DUPLEX
-  cv2.putText(frame,name, (left + 6, bottom - 6), font, 0.5,(255,255,255), 1)
+  for (top,right,bottom,left), name in zip(face_locations, face_names):
+      if not name:
+        continue
+      cv2.rectangle(frame,(left,top),(right,bottom),(0, 0, 255), 2)
+      cv2.rectangle(frame,(left,bottom-25),(right,bottom), (0,0,255), cv2.FILLED)
+      font =cv2.FONT_HERSHEY_DUPLEX
+      cv2.putText(frame,name, (left + 6, bottom - 6), font, 0.5,(255,255,255), 1)
 
-print("Writing frame {} / {}".format(frame_number,lenght))
-output_movie.write(frame)
+  print("Writing frame {} / {}".format(frame_number,lenght))
+  output_movie.write(frame)
 
 
 output_movie.release()
